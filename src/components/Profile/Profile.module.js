bac25780f4648a07af19dd66d6e8bdbda92e262e
nataprofits <nataprofits@gmail.com>
2023-05-27 13:32:14 +0100

    3

--- src/components/Profile/Profile.module.js
+++ src/components/Profile/Profile.module.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const Description = styled.div`
-  background-color: purple;
+  background-color: violet;
   padding: 10px;
   width: 300px;
   margin-right: auto;
