89ea78ce15189c5653213624e2a97672fdd8e520
nataprofits <nataprofits@gmail.com>
2023-05-27 17:31:05 +0100

    color

--- src/components/Profile/Profile.module.js
+++ src/components/Profile/Profile.module.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const Description = styled.div`
-  background-color: blueviolet;
+  background-color: white;
   padding: 10px;
   width: 300px;
   margin-right: auto;
